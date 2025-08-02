ec21fe3dbc7b9b63a21c422ea626b7dcb9a823af
Satyaa Goyal <satyaagoyal333333@gmail.com>
2025-08-02 17:14:17 +0530

    Added android native build

--- next.config.ts
+++ next.config.ts
@@ -10,7 +10,10 @@ setupDevPlatform().catch(console.error);
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
