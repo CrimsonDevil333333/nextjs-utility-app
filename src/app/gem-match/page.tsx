98e6c892cfade13a1d3078267f58028866a59363
Satyaa Goyal <satyaagoyal333333@gmail.com>
2025-08-11 00:14:27 +0530

    fixed page ui

--- src/app/gem-match/page.tsx
+++ src/app/gem-match/page.tsx
@@ -157,7 +157,7 @@ const GemMatchGame = () => {
     };
 
     return (
-        <div className="min-h-[calc(100vh-100px)] w-full flex flex-col items-center justify-center bg-gray-50 dark:bg-gray-900 p-4">
+        <div className="min-h-[calc(100vh-4rem)] w-full flex flex-col items-center justify-center bg-gray-50 dark:bg-gray-900 p-4">
             <div className="w-full max-w-md mx-auto">
                 <div className="text-center mb-4">
                     <h1 className="text-3xl sm:text-4xl font-extrabold text-gray-900 dark:text-white">Gem Match</h1>
